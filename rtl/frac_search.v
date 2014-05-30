1acdb51726d8d8af6579480891a4bcfabc074a65
Gabriel Teixeira <gabrieldiegoteixeira@gmail.com>
2014-05-30 16:17:03 -0300

    Entradas e saídas

--- rtl/frac_search.v
+++ rtl/frac_search.v
@@ -1,14 +1,36 @@
-module frac_search(in, out, clk, reset);
+module frac_search(filter_pix, ref_pix, in, out, clk, reset);
+  input [127:0] filter_pix;
+  input [63:0] ref_pix;
   input [7:0] in;
   output [7:0] out;
   input clk,reset;
 
+  wire [127:0] filter_pix;
+  wire [63:0] ref_pix;
   wire [7:0] in;
-  reg [7:0]  out;
-  wire  clk,reset;
+  reg  [7:0] out;
+  wire clk, reset;
+
+  reg [127:0] pix_buffer[3:0];
+  reg [3:0]   pix_counter;
+
+  /*
+   States of the FSM:
+   1-Ready to receive another block to filter
+   2-Receiving lines of block to filter but not ref block
+     -Should fill the number of taps to start calculation
+   3-Receiving lines of block to filter and ref block
+   4-Calculation ready
+   */
 
   always @(posedge clk)
-  begin
-    out <= in+1;
-  end
+    if (reset)
+      pix_counter <= 0;
+    else
+      begin
+        out <= in+1;
+        pix_buffer[pix_counter][7:0] <= filter_pix;
+        pix_counter = pix_counter+1;
+      end
+
 endmodule
--- rtl/frac_search_tb.v
+++ rtl/frac_search_tb.v
@@ -2,22 +2,47 @@ module frac_search_tb;
 
   reg [7:0] in=0;
   reg reset=0;
+  reg [127:0] filter_pix;
+  reg [63:0] ref_pix;
+
+  integer tmp;
+
+  parameter STDIN = 32'h8000_0000;
 
   initial
     begin
-      #2 in = 4;
-      #8 in = 17;
-      #10 $finish();
+      @(posedge clk)
+        reset = 1;
+
+      @(negedge clk)
+        reset = 0;
+
+      forever
+        begin
+          @(posedge clk)
+            begin
+              tmp = $fgetc(STDIN);
+              in = tmp;
+              if(in == 255)
+                $finish;
+            end
+        end
     end
 
   wire [7:0] value;
 
-  frac_search fs(in,value,clk,reset);
+  frac_search fs(filter_pix,ref_pix,in,value,clk,reset);
 
   reg clk = 0;
   always #1 clk = !clk;
 
   initial
     $monitor("At time %t value %d",$time,value);
+
+  initial
+    begin
+      $dumpfile("test.vcd");
+      $dumpvars(0,fs);
+    end
 
 endmodule
