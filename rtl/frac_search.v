58863cac13dc5d6319ad8ceee16225bdb8290f12
Gabriel Teixeira <gabrieldiegoteixeira@gmail.com>
2014-06-13 15:27:49 -0300

    Abordagem para o RTL simplificada

--- rtl/frac_search.v
+++ rtl/frac_search.v
@@ -1,46 +1,82 @@
-module frac_search(filter_pix, ref_pix, in, out, clk, reset);
-  input [127:0] filter_pix;
+module frac_search(filter_pix, ref_pix, input_ready, mvx, mvy, clk, reset);
+  input [63:0] filter_pix;
   input [63:0] ref_pix;
-  input [7:0] in;
-  output [7:0] out;
-  input clk,reset;
+  input        input_ready;
+  output [2:0] mvx, mvy;
 
-  wire [127:0] filter_pix;
+  input clk, reset;
+
+  wire [63:0] filter_pix;
   wire [63:0] ref_pix;
-  wire [7:0] in;
-  reg  [7:0] out;
+  reg  [2:0] mvx, mvy;
+
   wire clk, reset;
 
-  reg [127:0] pix_buffer[2:0];
-  reg [3:0]   pix_counter;
+  reg [63:0] pix_buffer;
+  reg [2:0] pix_counter;
 
   parameter HEIGHT = 8;
-  parameter TAPS = 8;
-  parameter HALF_TAPS = 3; // Index of the center of the filter
 
-  parameter PADDED_CU = 16;
-
-  reg [2:0]   state;
-  parameter IDLE = 0,RECP = 1;
+  reg [1:0] state,next_state;
+  parameter IDLE = 0,
+            RECV = 1;
 
   /*
-   States of the FSM:
-   1-Ready to receive another block to filter
-   2-Receiving lines of block to filter but not ref block
-     -Should fill the number of taps to start calculation
-   3-Receiving lines of block to filter and ref block
-   4-Calculation ready
+     The idea here is to perform the QPEL mv search in a 8x8 block
+      using only the reference 8x8 block and a 8x8 block to be filtered.
+     This reduces greatly the number of taps and does not require padding.
+     Thus there will be only two states:
+     -IDLE: Obvious
+     -RECV: 8 cycles to receive each line of both blocks
+     The result will be displayed the next cycle after it finishes receiving the blocks
    */
+
+  wire [7:0] filter_array[7:0]; // Can't use vector as input, so we declare as a wire
+  wire [7:0] ref_array[7:0];
+
+  assign {filter_array[7],filter_array[6],filter_array[5],filter_array[4],
+          filter_array[3],filter_array[2],filter_array[1],filter_array[0]} = filter_pix;
+
+  assign {ref_array[7],ref_array[6],ref_array[5],ref_array[4],
+          ref_array[3],ref_array[2],ref_array[1],ref_array[0]} = ref_pix;
 
   always @(posedge clk)
     if (reset == 1'b1) begin
-      pix_counter <= 0;
       state <= IDLE;
+      next_state <= IDLE;
+      pix_counter <= 0;
+      mvx <= 0;
+      mvy <= 0;
     end
     else begin
-      out <= in+1;
-      pix_buffer[pix_counter][127:0] <= filter_pix;
-      pix_counter = pix_counter+1;
+      state <= next_state;
     end
+
+  always @(posedge clk) begin
+    case(state)
+      IDLE: begin
+        mvx <= filter_pix[1:0];
+        mvy <= ref_pix[1:0];
+        pix_buffer = filter_pix;
+
+        if(input_ready)
+          next_state <= RECV;
+        else
+          next_state <= IDLE;
+      end
+      RECV: begin
+        mvx <= pix_counter[1:0];
+        mvy <= pix_counter[2:1];
+
+        pix_buffer = filter_pix;
+        pix_counter = pix_counter+1;
+
+        if(pix_counter == 0)
+          next_state <= IDLE;
+        else
+          next_state <= RECV;
+      end
+    endcase
+  end
 
 endmodule
--- rtl/frac_search_tb.v
+++ rtl/frac_search_tb.v
@@ -2,13 +2,26 @@ module frac_search_tb;
 
   reg [7:0] in=0;
   reg reset=0;
-  reg [127:0] filter_pix;
-  reg [63:0] ref_pix;
 
+  wire [63:0] filter_pix;
+  wire [63:0] ref_pix;
   integer tmp;
   integer c;
+  integer i;
 
   parameter STDIN = 32'h8000_0000;
+
+  wire [2:0] mvx, mvy;
+
+
+  reg [7:0] filter_array[7:0];
+  reg [7:0] ref_array[7:0];
+
+  assign filter_pix = {filter_array[7],filter_array[6],filter_array[5],filter_array[4],
+                       filter_array[3],filter_array[2],filter_array[1],filter_array[0]};
+
+  assign ref_pix = {ref_array[7],ref_array[6],ref_array[5],ref_array[4],
+                    ref_array[3],ref_array[2],ref_array[1],ref_array[0]};
 
   initial begin
     @(posedge clk)
@@ -19,23 +32,33 @@ module frac_search_tb;
 
     forever begin
       @(posedge clk) begin
-        c = $fscanf(STDIN,"%d",tmp);
-        in = tmp;
-        if(c != 1)
-          $finish;
+        for(i=0; i<8; i=i+1) begin
+          c = $fscanf(STDIN,"%x",tmp);
+          if(c != 1)
+            $finish;
+
+          filter_array[i] = tmp;
+        end
+
+        for(i=0; i<8; i=i+1) begin
+          c = $fscanf(STDIN,"%x",tmp);
+          if(c != 1)
+            $finish;
+
+          ref_array[i] = tmp;
+        end
+
       end
     end
   end
 
-  wire [7:0] value;
-
-  frac_search fs(filter_pix,ref_pix,in,value,clk,reset);
+  frac_search fs(filter_pix,ref_pix,1'b1,mvx,mvy,clk,reset);
 
   reg clk = 0;
   always #1 clk = !clk;
 
   initial
-    $monitor("At time %t value %d",$time,value);
+    $monitor("At time %t value %d %d",$time,mvx,mvy);
 
   initial begin
     $dumpfile("test.vcd");
