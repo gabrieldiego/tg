091c19b17fe40476833100eb4d4d058446be0c69
Gabriel Diego Teixeira <gabrieldiegoteixeira@gmail.com>
2014-08-05 10:43:33 -0300

    Corrigida a largura das portas de saída

--- rtl/abs_diff_line.v
+++ rtl/abs_diff_line.v
@@ -1,37 +1,47 @@
 module abs_diff_line(cur_upper_pix, cur_middle_pix, cur_lower_pix, org_pix,
-                     diff_UH_h, diff_UH_q, diff_UH_f,
-                     diff_UQ_h, diff_UQ_q, diff_UQ_f,
-                     diff_M_h , diff_M_q , diff_M_f ,
-                     diff_LQ_h, diff_LQ_q, diff_LQ_f,
-                     diff_LH_h, diff_LH_q, diff_LH_f);
+                     diff_UH_h, diff_UH_q, diff_UH_f, diff_UH_r, diff_UH_i,
+                     diff_UQ_h, diff_UQ_q, diff_UQ_f, diff_UQ_r, diff_UQ_i,
+                     diff_M_h , diff_M_q , diff_M_f , diff_M_r , diff_M_i ,
+                     diff_LQ_h, diff_LQ_q, diff_LQ_f, diff_LQ_r, diff_LQ_i,
+                     diff_LH_h, diff_LH_q, diff_LH_f, diff_LH_r, diff_LH_i);
 
   input [63:0] cur_upper_pix;
   input [63:0] cur_middle_pix;
   input [63:0] cur_lower_pix;
   input [63:0] org_pix;
 
-  output [111:0] diff_UH_h;
-  output [111:0] diff_UH_q;
-  output [ 55:0] diff_UH_f;
+  output [47:0] diff_UH_h;
+  output [47:0] diff_UH_q;
+  output [47:0] diff_UH_f;
+  output [47:0] diff_UH_r;
+  output [47:0] diff_UH_i;
 
-  output [111:0] diff_UQ_h;
-  output [111:0] diff_UQ_q;
-  output [ 55:0] diff_UQ_f;
+  output [47:0] diff_UQ_h;
+  output [47:0] diff_UQ_q;
+  output [47:0] diff_UQ_f;
+  output [47:0] diff_UQ_r;
+  output [47:0] diff_UQ_i;
 
-  output [111:0] diff_M_h;
-  output [111:0] diff_M_q;
-  output [ 55:0] diff_M_f;
+  output [47:0] diff_M_h;
+  output [47:0] diff_M_q;
+  output [47:0] diff_M_f;
+  output [47:0] diff_M_r;
+  output [47:0] diff_M_i;
 
-  output [111:0] diff_LQ_h;
-  output [111:0] diff_LQ_q;
-  output [ 55:0] diff_LQ_f;
+  output [47:0] diff_LQ_h;
+  output [47:0] diff_LQ_q;
+  output [47:0] diff_LQ_f;
+  output [47:0] diff_LQ_r;
+  output [47:0] diff_LQ_i;
 
-  output [111:0] diff_LH_h;
-  output [111:0] diff_LH_q;
-  output [ 55:0] diff_LH_f;
+  output [47:0] diff_LH_h;
+  output [47:0] diff_LH_q;
+  output [47:0] diff_LH_f;
+  output [47:0] diff_LH_r;
+  output [47:0] diff_LH_i;
 
   // Follow the next diagram:
-  // f   h q f q h   f <-- Index
+  // f   h q f r i   f <-- Index
   //
   // o   + + o + +   o <-- UF = Upper Full
   //
@@ -57,27 +67,34 @@ module abs_diff_line(cur_upper_pix, cur_middle_pix, cur_lower_pix, org_pix,
   // All subpixels are only calculated in 7 or 14 positions, thus the width of
   //  56 or 112 bits. The only exception is UH_f and LH_F because the extra
   //  result is needed for for the calculation of other sub-pixels
+  //
+  // Since the SAD is being calculated only for the inner 6x6 block of a 8x8
+  //  one, the outputs only need to be 48 or 96 bits wide. The extra position
+  //  is used only to allow the movement inside the block
 
-  wire [55:0]    UF_h;            
-  wire [111:0]   UF_q;
+  wire [55:0]     UF_h;            
+  wire [111:0]    UF_q;
 
-  wire [55:0]    M_h;            
-  wire [111:0]   M_q;
+  wire [55:0]     M_h;
+  wire [111:0]    M_q;
 
-  wire [55:0]    LF_h;            
-  wire [111:0]   LF_q;
+  wire [55:0]     LF_h;
+  wire [111:0]    LF_q;
 
-  filter_half    fhu(cur_upper_pix,UF_h);
-  filter_quarter fqu(cur_upper_pix,UF_q);
+  filter_half     fhu(cur_upper_pix,UF_h);
+  filter_quarter  fqu(cur_upper_pix,UF_q);
 
-  filter_half    fhm(cur_middle_pix,M_h);
-  filter_quarter fqm(cur_middle_pix,M_q);
+  filter_half     fhm(cur_middle_pix,M_h);
+  filter_quarter  fqm(cur_middle_pix,M_q);
 
-  filter_half    fhl(cur_lower_pix,LF_h);
-  filter_quarter fql(cur_lower_pix,LF_q);
+  filter_half     fhl(cur_lower_pix,LF_h);
+  filter_quarter  fql(cur_lower_pix,LF_q);
 
   wire [63:0]     UH_f;
+  wire [63:0]     M_f;
   wire [63:0]     LH_f;
+
+  assign M_f = cur_middle_pix;
 
   genvar          i;
   generate
@@ -97,11 +114,11 @@ module abs_diff_line(cur_upper_pix, cur_middle_pix, cur_lower_pix, org_pix,
     end
   endgenerate
 
-  wire [111:0]    UH_q;
-  wire [111:0]    LH_q;
+  wire [95:0]     UH_q;
+  wire [95:0]     LH_q;
 
   generate
-    for(i=0;i<56;i=i+8) begin: UH_LH_q_gen
+    for(i=0;i<48;i=i+8) begin: UH_LH_q_gen
       assign UH_q[2*i+ 7:2*i  ] = (UH_f[i+ 7:i   ] + 3*UH_f[i+15:i+8]) >> 2;
       assign UH_q[2*i+15:2*i+8] = (UH_f[i+23:i+16] + 3*UH_f[i+15:i+8]) >> 2;
 
@@ -110,57 +127,76 @@ module abs_diff_line(cur_upper_pix, cur_middle_pix, cur_lower_pix, org_pix,
     end
   endgenerate
 
-  wire [55:0]     UQ_h;
-  wire [55:0]     LQ_h;
-  wire [111:0]    UQ_q;
-  wire [111:0]    LQ_q;
+
+  wire [47:0]     UQ_f;
+  wire [47:0]     LQ_f;
 
   generate
-    for(i=0;i<56;i=i+8) begin: UQ_LQ_h_q_gen
+    for(i=0;i<48;i=i+8) begin: UQ_LQ_f_gen
+      assign UQ_f[i+7:i]=(cur_upper_pix[i+15:i+8]+cur_middle_pix[i+15:i]+8)>>1;
+      assign LQ_f[i+7:i]=(cur_lower_pix[i+15:i+8]+cur_middle_pix[i+15:i+8])>>1;
+    end
+  endgenerate
+
+  wire [55:0]     UQ_h;
+  wire [55:0]     LQ_h;
+
+  generate
+    for(i=0;i<56;i=i+8) begin: UQ_LQ_h_gen
       assign UQ_h[i+7:i] = (UF_h[i+7:i] + 3*M_h[i+7:i]) >> 2;
       assign LQ_h[i+7:i] = (LF_h[i+7:i] + 3*M_h[i+7:i]) >> 2;
+    end
+  endgenerate
 
-      assign UQ_q[2*i+ 7:2*i  ] = (UF_q[2*i+ 7:2*i  ] + 3*M_q[2*i+ 7:2*i  ]) >> 2;
-      assign UQ_q[2*i+15:2*i+8] = (UF_q[2*i+15:2*i+8] + 3*M_q[2*i+15:2*i+8]) >> 2;
+  wire [47:0]    UQ_q;
+  wire [47:0]    LQ_q;
 
-      assign LQ_q[2*i+ 7:2*i  ] = (LF_q[2*i+ 7:2*i  ] + 3*M_q[2*i+ 7:2*i  ]) >> 2;
-      assign LQ_q[2*i+15:2*i+8] = (LF_q[2*i+15:2*i+8] + 3*M_q[2*i+15:2*i+8]) >> 2;
+  wire [47:0]    UQ_r;
+  wire [47:0]    LQ_r;
+
+  generate
+    for(i=0;i<48;i=i+8) begin: UQ_LQ_q_gen
+      assign UQ_q[i+7:i] = (UF_q[2*i+ 7:2*i  ] + 3*M_q[2*i+7:2*i]) >> 2;
+      assign UQ_r[i+7:i] = (UF_q[2*i+15:2*i+8] + 3*M_q[2*i+7:2*i]) >> 2;
+
+      assign LQ_q[i+7:i] = (LF_q[2*i+ 7:2*i  ] + 3*M_q[2*i+7:2*i]) >> 2;
+      assign LQ_q[i+7:i] = (LF_q[2*i+15:2*i+8] + 3*M_q[2*i+7:2*i]) >> 2;
     end
   endgenerate
 
   generate
-    for(i=0; i<56; i=i+8) begin: abs_diff_generate
-      abs_diff ad_UH_h(org_pix[i+ 7:i  ],UH_h[  i+ 7:  i  ],diff_UH_h[2*i+ 7:2*i  ]);
-      abs_diff ad_UH_i(org_pix[i+15:i+8],UH_h[  i+15:  i+8],diff_UH_h[2*i+15:2*i+8]);
-      abs_diff ad_UH_q(org_pix[i+ 7:i  ],UH_q[2*i+ 7:2*i  ],diff_UH_q[2*i+ 7:2*i  ]);
-      abs_diff ad_UH_r(org_pix[i+15:i+8],UH_q[2*i+15:2*i+8],diff_UH_q[2*i+15:2*i+8]);
-      abs_diff ad_UH_f(org_pix[i+7 :i  ],UH_h[  i+7 :  i  ],diff_UH_f[  i +7:  i  ]);
+    for(i=0; i<48; i=i+8) begin: abs_diff_generate
+      abs_diff ad_UH_h(org_pix[i+15:i+8],UH_h[  i+ 7:  i  ],diff_UH_h[i+7:i]);
+      abs_diff ad_UH_i(org_pix[i+15:i+8],UH_h[  i+15:  i+8],diff_UH_i[i+7:i]);
+      abs_diff ad_UH_q(org_pix[i+15:i+8],UH_q[2*i+ 7:2*i  ],diff_UH_q[i+7:i]);
+      abs_diff ad_UH_r(org_pix[i+15:i+8],UH_q[2*i+15:2*i+8],diff_UH_r[i+7:i]);
+      abs_diff ad_UH_f(org_pix[i+15:i+8],UH_f[  i+15:  i+8],diff_UH_f[i+7:i]);
 
-      abs_diff ad_UQ_h(org_pix[i+ 7:i  ],UQ_h[  i+ 7:  i  ],diff_UQ_h[2*i+ 7:2*i  ]);
-      abs_diff ad_UQ_i(org_pix[i+15:i+8],UQ_h[  i+15:  i+8],diff_UQ_h[2*i+15:2*i+8]);
-      abs_diff ad_UQ_q(org_pix[i+ 7:i  ],UQ_q[2*i+ 7:2*i  ],diff_UQ_q[2*i+ 7:2*i  ]);
-      abs_diff ad_UQ_r(org_pix[i+15:i+8],UQ_q[2*i+15:2*i+8],diff_UQ_q[2*i+15:2*i+8]);
-      abs_diff ad_UQ_f(org_pix[i+7 :i  ],UQ_h[  i+7 :  i  ],diff_UQ_f[  i +7:  i  ]);
-
-
-      abs_diff ad_M_h(org_pix[i+ 7:i  ], M_h[  i+ 7:  i  ], diff_M_h[2*i+ 7:2*i  ]);
-      abs_diff ad_M_i(org_pix[i+15:i+8], M_h[  i+15:  i+8], diff_M_h[2*i+15:2*i+8]);
-      abs_diff ad_M_q(org_pix[i+ 7:i  ], M_q[2*i+ 7:2*i  ], diff_M_q[2*i+ 7:2*i  ]);
-      abs_diff ad_M_r(org_pix[i+15:i+8], M_q[2*i+15:2*i+8], diff_M_q[2*i+15:2*i+8]);
-      abs_diff ad_M_f(org_pix[i+15:i+8],cur_middle_pix[i+15:i+8],diff_M_f[i+7:i  ]);
+      abs_diff ad_UQ_h(org_pix[i+15:i+8],UQ_h[  i+ 7:  i  ],diff_UQ_h[i+7:i]);
+      abs_diff ad_UQ_i(org_pix[i+15:i+8],UQ_h[  i+15:  i+8],diff_UQ_i[i+7:i]);
+      abs_diff ad_UQ_q(org_pix[i+15:i+8],UQ_q[2*i+ 7:2*i  ],diff_UQ_q[i+7:i]);
+      abs_diff ad_UQ_r(org_pix[i+15:i+8],UQ_q[2*i+15:2*i+8],diff_UQ_r[i+7:i]);
+      abs_diff ad_UQ_f(org_pix[i+15:i+8],UQ_f[  i+ 7:  i  ],diff_UQ_f[i+7:i]);
 
 
-      abs_diff ad_LQ_h(org_pix[i+ 7:i  ],LQ_h[  i+ 7:  i  ],diff_LQ_h[2*i+ 7:2*i  ]);
-      abs_diff ad_LQ_i(org_pix[i+15:i+8],LQ_h[  i+15:  i+8],diff_LQ_h[2*i+15:2*i+8]);
-      abs_diff ad_LQ_q(org_pix[i+ 7:i  ],LQ_q[2*i+ 7:2*i  ],diff_LQ_q[2*i+ 7:2*i  ]);
-      abs_diff ad_LQ_r(org_pix[i+15:i+8],LQ_q[2*i+15:2*i+8],diff_LQ_q[2*i+15:2*i+8]);
-      abs_diff ad_LQ_f(org_pix[i+7 :i  ],LQ_h[  i+7 :  i  ],diff_LQ_f[  i +7:  i  ]);
+      abs_diff ad_M_h(org_pix[i+15:i+8], M_h[  i+ 7:  i  ], diff_M_h[i+7:i]);
+      abs_diff ad_M_i(org_pix[i+15:i+8], M_h[  i+15:  i+8], diff_M_i[i+7:i]);
+      abs_diff ad_M_q(org_pix[i+15:i+8], M_q[2*i+ 7:2*i  ], diff_M_q[i+7:i]);
+      abs_diff ad_M_r(org_pix[i+15:i+8], M_q[2*i+15:2*i+8], diff_M_r[i+7:i]);
+      abs_diff ad_M_f(org_pix[i+15:i+8], M_f[  i+15:  i+8], diff_M_f[i+7:i]);
 
-      abs_diff ad_LH_h(org_pix[i+ 7:i  ],LH_h[  i+ 7:  i  ],diff_LH_h[2*i+ 7:2*i  ]);
-      abs_diff ad_LH_i(org_pix[i+15:i+8],LH_h[  i+15:  i+8],diff_LH_h[2*i+15:2*i+8]);
-      abs_diff ad_LH_q(org_pix[i+ 7:i  ],LH_q[2*i+ 7:2*i  ],diff_LH_q[2*i+ 7:2*i  ]);
-      abs_diff ad_LH_r(org_pix[i+15:i+8],LH_q[2*i+15:2*i+8],diff_LH_q[2*i+15:2*i+8]);
-      abs_diff ad_LH_f(org_pix[i+7 :i  ],LH_h[  i+7 :  i  ],diff_LH_f[  i +7:  i  ]);
+
+      abs_diff ad_LQ_h(org_pix[i+15:i+8],LQ_h[  i+ 7:  i  ],diff_LQ_h[i+7:i]);
+      abs_diff ad_LQ_i(org_pix[i+15:i+8],LQ_h[  i+15:  i+8],diff_LQ_i[i+7:i]);
+      abs_diff ad_LQ_q(org_pix[i+15:i+8],LQ_q[2*i+ 7:2*i  ],diff_LQ_q[i+7:i]);
+      abs_diff ad_LQ_r(org_pix[i+15:i+8],LQ_q[2*i+15:2*i+8],diff_LQ_r[i+7:i]);
+      abs_diff ad_LQ_f(org_pix[i+15:i+8],LQ_f[  i+ 7:  i  ],diff_LQ_f[i+7:i]);
+
+      abs_diff ad_LH_h(org_pix[i+15:i+8],LH_h[  i+ 7:  i  ],diff_LH_h[i+7:i]);
+      abs_diff ad_LH_i(org_pix[i+15:i+8],LH_h[  i+15:  i+8],diff_LH_i[i+7:i]);
+      abs_diff ad_LH_q(org_pix[i+15:i+8],LH_q[2*i+ 7:2*i  ],diff_LH_q[i+7:i]);
+      abs_diff ad_LH_r(org_pix[i+15:i+8],LH_q[2*i+15:2*i+8],diff_LH_r[i+7:i]);
+      abs_diff ad_LH_f(org_pix[i+15:i+8],LH_f[  i+15:  i+8],diff_LH_f[i+7:i]);
 
     end
   endgenerate
--- rtl/abs_diff_line_tb.v
+++ rtl/abs_diff_line_tb.v
@@ -24,25 +24,38 @@ module abs_diff_line_tb;
   reg [7:0]   cur_array[7:0];
   reg [7:0]   org_array[7:0];
 
-  wire [111:0] diff_UH_h;
-  wire [111:0] diff_UH_q;
-  wire [ 55:0] diff_UH_f;
+  reg [7:0]   s_array[5:0];
+  reg [7:0]   d_array[11:0];
 
-  wire [111:0] diff_UQ_h;
-  wire [111:0] diff_UQ_q;
-  wire [ 55:0] diff_UQ_f;
+  wire [47:0] diff_UH_h;
+  wire [47:0] diff_UH_q;
+  wire [47:0] diff_UH_f;
+  wire [47:0] diff_UH_r;
+  wire [47:0] diff_UH_i;
 
-  wire [111:0] diff_M_h;
-  wire [111:0] diff_M_q;
-  wire [ 55:0] diff_M_f;
+  wire [47:0] diff_UQ_h;
+  wire [47:0] diff_UQ_q;
+  wire [47:0] diff_UQ_f;
+  wire [47:0] diff_UQ_r;
+  wire [47:0] diff_UQ_i;
 
-  wire [111:0] diff_LQ_h;
-  wire [111:0] diff_LQ_q;
-  wire [ 55:0] diff_LQ_f;
+  wire [47:0] diff_M_h;
+  wire [47:0] diff_M_q;
+  wire [47:0] diff_M_f;
+  wire [47:0] diff_M_r;
+  wire [47:0] diff_M_i;
 
-  wire [111:0] diff_LH_h;
-  wire [111:0] diff_LH_q;
-  wire [ 55:0] diff_LH_f;
+  wire [47:0] diff_LQ_h;
+  wire [47:0] diff_LQ_q;
+  wire [47:0] diff_LQ_f;
+  wire [47:0] diff_LQ_r;
+  wire [47:0] diff_LQ_i;
+
+  wire [47:0] diff_LH_h;
+  wire [47:0] diff_LH_q;
+  wire [47:0] diff_LH_f;
+  wire [47:0] diff_LH_r;
+  wire [47:0] diff_LH_i;
 
 
   initial begin
@@ -104,6 +117,31 @@ module abs_diff_line_tb;
     cur_lower_pix = {cur_array[7],cur_array[6],cur_array[5],cur_array[4],
                      cur_array[3],cur_array[2],cur_array[1],cur_array[0]};
 
+
+    {cur_array[7],cur_array[6],cur_array[5],cur_array[4],
+     cur_array[3],cur_array[2],cur_array[1],cur_array[0]} = cur_upper_pix;
+
+    for(i=0;i<8;i=i+1) begin
+      $write("%x ",cur_array[i]);
+    end
+    $write("\n");
+
+    {cur_array[7],cur_array[6],cur_array[5],cur_array[4],
+     cur_array[3],cur_array[2],cur_array[1],cur_array[0]} = cur_middle_pix;
+
+    for(i=0;i<8;i=i+1) begin
+      $write("%x ",cur_array[i]);
+    end
+    $write("\n");
+
+    {cur_array[7],cur_array[6],cur_array[5],cur_array[4],
+     cur_array[3],cur_array[2],cur_array[1],cur_array[0]} = cur_lower_pix;
+
+    for(i=0;i<8;i=i+1) begin
+      $write("%x ",cur_array[i]);
+    end
+    $write("\n\n");
+
     for(i=0;i<8;i=i+1) begin
       c = $fscanf(org_file,"%x",tmp);
       if(c != 1) begin
@@ -115,25 +153,22 @@ module abs_diff_line_tb;
     org_pix = {org_array[7],org_array[6],org_array[5],org_array[4],
                org_array[3],org_array[2],org_array[1],org_array[0]};
 
-/*
-    #1 for(i=0;i<7;i=i+1) begin
-      $write("%2x ",half_array[i]);
+    for(i=0;i<8;i=i+1) begin
+      $write("%x ",org_array[i]);
     end
-    $write("\n");
+    $write("\n\n");
 
-    for(i=0;i<14;i=i+1) begin
-      $write("%2x ",quarter_array[i]);
-    end
-    $write("\n");*/
+    #1 $write("%2x %2x %2x %2x %2x %2x\n", diff_M_f[ 7: 0],diff_M_f[15: 8],
+           diff_M_f[23:16],diff_M_f[31:24],diff_M_f[39:32],diff_M_f[47:40]);
 
   end
 
   abs_diff_line  abl(cur_upper_pix, cur_middle_pix ,cur_lower_pix, org_pix,
-                     diff_UH_h, diff_UH_q, diff_UH_f,
-                     diff_UQ_h, diff_UQ_q, diff_UQ_f,
-                     diff_M_h , diff_M_q , diff_M_f ,
-                     diff_LQ_h, diff_LQ_q, diff_LQ_f,
-                     diff_LH_h, diff_LH_q, diff_LH_f);
+                     diff_UH_h, diff_UH_q, diff_UH_f, diff_UH_r, diff_UH_i,
+                     diff_UQ_h, diff_UQ_q, diff_UQ_f, diff_UQ_r, diff_UQ_i,
+                     diff_M_h , diff_M_q , diff_M_f , diff_M_r , diff_M_i ,
+                     diff_LQ_h, diff_LQ_q, diff_LQ_f, diff_LQ_r, diff_LQ_i,
+                     diff_LH_h, diff_LH_q, diff_LH_f, diff_LH_r, diff_LH_i);
 
   initial begin
     $dumpfile("abs_diff_line.vcd");
