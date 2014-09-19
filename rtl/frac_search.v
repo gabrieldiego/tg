2e60f27123b0be3925009635a59026754c746a3c
Gabriel Teixeira <gabrieldiegoteixeira@gmail.com>
2014-09-18 23:22:47 -0300

    Maiores correções na máquina de estados

--- rtl/frac_search.v
+++ rtl/frac_search.v
@@ -63,11 +63,11 @@ module frac_search(cur_pix, org_pix, ready, sad_out, mvx, mvy, clk, reset);
   reg  [13:0]   sad_LQ[4:0];
   reg  [13:0]   sad_LH[4:0];
 
-  reg  [13:0]   next_sad_UH[4:0];
-  reg  [13:0]   next_sad_UQ[4:0];
-  reg  [13:0]   next_sad_M[4:0];
-  reg  [13:0]   next_sad_LQ[4:0];
-  reg  [13:0]   next_sad_LH[4:0];
+  wire [13:0]   next_sad_UH[4:0];
+  wire [13:0]   next_sad_UQ[4:0];
+  wire [13:0]   next_sad_M[4:0];
+  wire [13:0]   next_sad_LQ[4:0];
+  wire [13:0]   next_sad_LH[4:0];
 
   wire [69:0]   sad_UH_vec;
   wire [69:0]   sad_UQ_vec;
@@ -85,11 +85,11 @@ module frac_search(cur_pix, org_pix, ready, sad_out, mvx, mvy, clk, reset);
   compute_sad cs(cur_upper_pix, cur_middle_pix, cur_lower_pix, org_pix_reg,
        line_sad_UH, line_sad_UQ, line_sad_M, line_sad_LQ, line_sad_LH);
 
-  assign sad_UH_vec = {sad_UH[4], sad_UH[3], sad_UH[2], sad_UH[1], sad_UH[0]};
-  assign sad_UQ_vec = {sad_UQ[4], sad_UQ[3], sad_UQ[2], sad_UQ[1], sad_UQ[0]};
-  assign sad_M_vec  = {sad_M[4] , sad_M[3] , sad_M[2] , sad_M[1] , sad_M[0] };
-  assign sad_LQ_vec = {sad_LQ[4], sad_LQ[3], sad_LQ[2], sad_LQ[1], sad_LQ[0]};
-  assign sad_LH_vec = {sad_LH[4], sad_LH[3], sad_LH[2], sad_LH[1], sad_LH[0]};
+  assign sad_UH_vec = {next_sad_UH[4], next_sad_UH[3], next_sad_UH[2], next_sad_UH[1], next_sad_UH[0]};
+  assign sad_UQ_vec = {next_sad_UQ[4], next_sad_UQ[3], next_sad_UQ[2], next_sad_UQ[1], next_sad_UQ[0]};
+  assign sad_M_vec  = {next_sad_M[4] , next_sad_M[3] , next_sad_M[2] , next_sad_M[1] , next_sad_M[0] };
+  assign sad_LQ_vec = {next_sad_LQ[4], next_sad_LQ[3], next_sad_LQ[2], next_sad_LQ[1], next_sad_LQ[0]};
+  assign sad_LH_vec = {next_sad_LH[4], next_sad_LH[3], next_sad_LH[2], next_sad_LH[1], next_sad_LH[0]};
 
   smallest_sad ss_UH(sad_UH_vec,smallest_sad_vec[13: 0],idx_min[0]);
   smallest_sad ss_UQ(sad_UQ_vec,smallest_sad_vec[27:14],idx_min[1]);
@@ -102,6 +102,37 @@ module frac_search(cur_pix, org_pix, ready, sad_out, mvx, mvy, clk, reset);
   assign mvy     = idx_min_ver;
   assign mvx     = idx_min[idx_min_ver];
   assign sad_out = smallest_sad_ver;
+
+  assign next_sad_UH[4] = sad_UH[4] + line_sad_UH[59:48];
+  assign next_sad_UH[3] = sad_UH[3] + line_sad_UH[47:36];
+  assign next_sad_UH[2] = sad_UH[2] + line_sad_UH[35:24];
+  assign next_sad_UH[1] = sad_UH[1] + line_sad_UH[23:12];
+  assign next_sad_UH[0] = sad_UH[0] + line_sad_UH[11: 0];
+
+  assign next_sad_UQ[4] = sad_UQ[4] + line_sad_UQ[59:48];
+  assign next_sad_UQ[3] = sad_UQ[3] + line_sad_UQ[47:36];
+  assign next_sad_UQ[2] = sad_UQ[2] + line_sad_UQ[35:24];
+  assign next_sad_UQ[1] = sad_UQ[1] + line_sad_UQ[23:12];
+  assign next_sad_UQ[0] = sad_UQ[0] + line_sad_UQ[11: 0];
+
+  assign next_sad_M[4]  = sad_M[4]  + line_sad_M[59:48];
+  assign next_sad_M[3]  = sad_M[3]  + line_sad_M[47:36];
+  assign next_sad_M[2]  = sad_M[2]  + line_sad_M[35:24];
+  assign next_sad_M[1]  = sad_M[1]  + line_sad_M[23:12];
+  assign next_sad_M[0]  = sad_M[0]  + line_sad_M[11: 0];
+
+  assign next_sad_LQ[4] = sad_LQ[4] + line_sad_LQ[59:48];
+  assign next_sad_LQ[3] = sad_LQ[3] + line_sad_LQ[47:36];
+  assign next_sad_LQ[2] = sad_LQ[2] + line_sad_LQ[35:24];
+  assign next_sad_LQ[1] = sad_LQ[1] + line_sad_LQ[23:12];
+  assign next_sad_LQ[0] = sad_LQ[0] + line_sad_LQ[11: 0];
+
+  assign next_sad_LH[4] = sad_LH[4] + line_sad_LH[59:48];
+  assign next_sad_LH[3] = sad_LH[3] + line_sad_LH[47:36];
+  assign next_sad_LH[2] = sad_LH[2] + line_sad_LH[35:24];
+  assign next_sad_LH[1] = sad_LH[1] + line_sad_LH[23:12];
+  assign next_sad_LH[0] = sad_LH[0] + line_sad_LH[11: 0];
+
 
   always @(posedge clk or posedge reset)
     if (reset == 1'b1) begin
@@ -117,58 +148,81 @@ module frac_search(cur_pix, org_pix, ready, sad_out, mvx, mvy, clk, reset);
     else begin
       state = next_state;
 
-      case(state)
-        IDLE: begin
-          cur_lower_pix  = cur_pix;
-          counter        = 0;
-        end
-        RECV: begin
-          if(ready) begin
-            cur_upper_pix  = cur_middle_pix;
-            cur_middle_pix = cur_lower_pix;
-            cur_lower_pix  = cur_pix;
-            org_pix_reg    = org_pix;
-            counter        = counter + 1;
+      if(state == IDLE) begin
+        cur_lower_pix  = cur_pix;
+        counter        = 0;
+      end
 
-            sad_UH[4] = next_sad_UH[4];
-            sad_UH[3] = next_sad_UH[3];
-            sad_UH[2] = next_sad_UH[2];
-            sad_UH[1] = next_sad_UH[1];
-            sad_UH[0] = next_sad_UH[0];
+      if((state == RECV && ready) || state == RSLT) begin
+        cur_upper_pix  = cur_middle_pix;
+        cur_middle_pix = cur_lower_pix;
+        cur_lower_pix  = cur_pix;
+        org_pix_reg    = org_pix;
+        counter        = counter + 1;
+      end
 
-            sad_UQ[4] = next_sad_UQ[4];
-            sad_UQ[3] = next_sad_UQ[3];
-            sad_UQ[2] = next_sad_UQ[2];
-            sad_UQ[1] = next_sad_UQ[1];
-            sad_UQ[0] = next_sad_UQ[0];
+      if((state == RECV && ready && counter > 3) || state == RSLT) begin
+        sad_UH[4] = next_sad_UH[4];
+        sad_UH[3] = next_sad_UH[3];
+        sad_UH[2] = next_sad_UH[2];
+        sad_UH[1] = next_sad_UH[1];
+        sad_UH[0] = next_sad_UH[0];
 
-            sad_M[4]  = next_sad_M[4];
-            sad_M[3]  = next_sad_M[3];
-            sad_M[2]  = next_sad_M[2];
-            sad_M[1]  = next_sad_M[1];
-            sad_M[0]  = next_sad_M[0];
+        sad_UQ[4] = next_sad_UQ[4];
+        sad_UQ[3] = next_sad_UQ[3];
+        sad_UQ[2] = next_sad_UQ[2];
+        sad_UQ[1] = next_sad_UQ[1];
+        sad_UQ[0] = next_sad_UQ[0];
 
-            sad_LQ[4] = next_sad_LQ[4];
-            sad_LQ[3] = next_sad_LQ[3];
-            sad_LQ[2] = next_sad_LQ[2];
-            sad_LQ[1] = next_sad_LQ[1];
-            sad_LQ[0] = next_sad_LQ[0];
+        sad_M[4]  = next_sad_M[4];
+        sad_M[3]  = next_sad_M[3];
+        sad_M[2]  = next_sad_M[2];
+        sad_M[1]  = next_sad_M[1];
+        sad_M[0]  = next_sad_M[0];
 
-            sad_LH[4] = next_sad_LH[4];
-            sad_LH[3] = next_sad_LH[3];
-            sad_LH[2] = next_sad_LH[2];
-            sad_LH[1] = next_sad_LH[1];
-            sad_LH[0] = next_sad_LH[0];
-          end
-        end
-        RSLT: begin
-          cur_upper_pix  = cur_middle_pix;
-          cur_middle_pix = cur_lower_pix;
-          cur_lower_pix  = cur_pix;
-          org_pix_reg    = org_pix;
-          counter = 0;
-        end
-      endcase
+        sad_LQ[4] = next_sad_LQ[4];
+        sad_LQ[3] = next_sad_LQ[3];
+        sad_LQ[2] = next_sad_LQ[2];
+        sad_LQ[1] = next_sad_LQ[1];
+        sad_LQ[0] = next_sad_LQ[0];
+
+        sad_LH[4] = next_sad_LH[4];
+        sad_LH[3] = next_sad_LH[3];
+        sad_LH[2] = next_sad_LH[2];
+        sad_LH[1] = next_sad_LH[1];
+        sad_LH[0] = next_sad_LH[0];
+      end
+      else begin
+        sad_UH[4] = 0;
+        sad_UH[3] = 0;
+        sad_UH[2] = 0;
+        sad_UH[1] = 0;
+        sad_UH[0] = 0;
+
+        sad_UQ[4] = 0;
+        sad_UQ[3] = 0;
+        sad_UQ[2] = 0;
+        sad_UQ[1] = 0;
+        sad_UQ[0] = 0;
+
+        sad_M[4]  = 0;
+        sad_M[3]  = 0;
+        sad_M[2]  = 0;
+        sad_M[1]  = 0;
+        sad_M[0]  = 0;
+
+        sad_LQ[4] = 0;
+        sad_LQ[3] = 0;
+        sad_LQ[2] = 0;
+        sad_LQ[1] = 0;
+        sad_LQ[0] = 0;
+
+        sad_LH[4] = 0;
+        sad_LH[3] = 0;
+        sad_LH[2] = 0;
+        sad_LH[1] = 0;
+        sad_LH[0] = 0;
+      end
   end
 
   always @(*) begin
@@ -185,68 +239,6 @@ module frac_search(cur_pix, org_pix, ready, sad_out, mvx, mvy, clk, reset);
 		end
         else begin
           next_state <= RECV;
-        end
-        if(counter > 2) begin
-          next_sad_UH[4] = sad_UH[4] + line_sad_UH[59:48];
-          next_sad_UH[3] = sad_UH[3] + line_sad_UH[47:36];
-          next_sad_UH[2] = sad_UH[2] + line_sad_UH[35:24];
-          next_sad_UH[1] = sad_UH[1] + line_sad_UH[23:12];
-          next_sad_UH[0] = sad_UH[0] + line_sad_UH[11: 0];
-
-          next_sad_UQ[4] = sad_UQ[4] + line_sad_UQ[59:48];
-          next_sad_UQ[3] = sad_UQ[3] + line_sad_UQ[47:36];
-          next_sad_UQ[2] = sad_UQ[2] + line_sad_UQ[35:24];
-          next_sad_UQ[1] = sad_UQ[1] + line_sad_UQ[23:12];
-          next_sad_UQ[0] = sad_UQ[0] + line_sad_UQ[11: 0];
-
-          next_sad_M[4]  = sad_M[4]  + line_sad_M[59:48];
-          next_sad_M[3]  = sad_M[3]  + line_sad_M[47:36];
-          next_sad_M[2]  = sad_M[2]  + line_sad_M[35:24];
-          next_sad_M[1]  = sad_M[1]  + line_sad_M[23:12];
-          next_sad_M[0]  = sad_M[0]  + line_sad_M[11: 0];
-
-          next_sad_LQ[4] = sad_LQ[4] + line_sad_LQ[59:48];
-          next_sad_LQ[3] = sad_LQ[3] + line_sad_LQ[47:36];
-          next_sad_LQ[2] = sad_LQ[2] + line_sad_LQ[35:24];
-          next_sad_LQ[1] = sad_LQ[1] + line_sad_LQ[23:12];
-          next_sad_LQ[0] = sad_LQ[0] + line_sad_LQ[11: 0];
-
-          next_sad_LH[4] = sad_LH[4] + line_sad_LH[59:48];
-          next_sad_LH[3] = sad_LH[3] + line_sad_LH[47:36];
-          next_sad_LH[2] = sad_LH[2] + line_sad_LH[35:24];
-          next_sad_LH[1] = sad_LH[1] + line_sad_LH[23:12];
-          next_sad_LH[0] = sad_LH[0] + line_sad_LH[11: 0];
-        end
-        else begin
-          next_sad_UH[4] = 0;
-          next_sad_UH[3] = 0;
-          next_sad_UH[2] = 0;
-          next_sad_UH[1] = 0;
-          next_sad_UH[0] = 0;
-
-          next_sad_UQ[4] = 0;
-          next_sad_UQ[3] = 0;
-          next_sad_UQ[2] = 0;
-          next_sad_UQ[1] = 0;
-          next_sad_UQ[0] = 0;
-
-          next_sad_M[4]  = 0;
-          next_sad_M[3]  = 0;
-          next_sad_M[2]  = 0;
-          next_sad_M[1]  = 0;
-          next_sad_M[0]  = 0;
-
-          next_sad_LQ[4] = 0;
-          next_sad_LQ[3] = 0;
-          next_sad_LQ[2] = 0;
-          next_sad_LQ[1] = 0;
-          next_sad_LQ[0] = 0;
-
-          next_sad_LH[4] = 0;
-          next_sad_LH[3] = 0;
-          next_sad_LH[2] = 0;
-          next_sad_LH[1] = 0;
-          next_sad_LH[0] = 0;
         end
       end
       RSLT: begin
